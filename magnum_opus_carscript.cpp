34a7bb6747ceb2d1c18def4396c08b9c050ad0b1
apollothecodemeleon <128416433+apollothecodemeleon@users.noreply.github.com>
2024-01-18 18:05:24 -0800

    made more progress on the car rental script, still have to add actual contents :(
    noding 💀 grammar

--- magnum_opus_carscript.cpp
+++ magnum_opus_carscript.cpp
@@ -1,13 +1,36 @@
 #include <iostream>
 using namespace std;
 
+class shortcutClass {
+	public:
+
+	void clearFunct(){
+		cout << "\033[2J\033[0;0H";
+	}
+	  
+}
 
 int main() {
+	shortcutClass shorcuts;
 	int carMenuOption;
+	shorcuts.clearFunct;
 	cout << "Welcome to Offrent, the car rental service of your dreams!";
-	cout << "\nPress Any Key to Continiue!";
+	cout << "\nPress Any Key to Continue!";
 	getchar;
+	shorcuts.clearFunct;
 	cout << "Please input 1 to rent cars, or 2 to turn in them in";
-	cin >> "carmenu option";
+	cin >> carMenuOption;
+
+	switch(carMenuOption) {
+
+		/*rental
+		section*/
+		case 1:  
+
+
+		/*Turn-In
+		section*/
+		case 2:
+	}
 	
 }
