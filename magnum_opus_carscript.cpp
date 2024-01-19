db4de2d79a882681366f72feacf0032b3e6521ad
apollothecodemeleon <128416433+apollothecodemeleon@users.noreply.github.com>
2024-01-18 17:34:22 -0800

    Updated magnum_opus_carscript.cpp
    SOooo basically i couldn't finish yesterday cuz i kept getting interuptted

--- magnum_opus_carscript.cpp
+++ magnum_opus_carscript.cpp
@@ -1,1 +1,10 @@
+#include <iostream>
+using namespace std;
 
+
+int main() {
+	cout << "Welcome to Offrent, the car rental service of your dreams!";
+	cout << "\nPress Any Key to Continiue!";
+	getchar;
+
+}
