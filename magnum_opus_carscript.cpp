14535ba0a2e568084cde8a1ea10ec644604bf4ac
apollothecodemeleon <128416433+apollothecodemeleon@users.noreply.github.com>
2024-01-18 17:43:58 -0800

    commiting stuff so i can open it in Vscode without that darn vs cursor block

--- magnum_opus_carscript.cpp
+++ magnum_opus_carscript.cpp
@@ -3,8 +3,11 @@ using namespace std;
 
 
 int main() {
+	int carMenuOption;
 	cout << "Welcome to Offrent, the car rental service of your dreams!";
 	cout << "\nPress Any Key to Continiue!";
 	getchar;
-
+	cout << "Please input 1 to rent cars, or 2 to turn in them in";
+	cin >> "carmenu option";
+	
 }
