af5d5be26bd276769c98c1a6b731be18ec792de6
apollothecodemeleon <128416433+apollothecodemeleon@users.noreply.github.com>
2024-01-19 18:31:19 -0800

    Updated magnum_opus_carscript.cpp
    i really had to push myself to do this
    im getting addicted to this
    i am tired
    i am hungry

--- magnum_opus_carscript.cpp
+++ magnum_opus_carscript.cpp
@@ -13,6 +13,7 @@ class shortcutClass {
 int main() {
 	shortcutClass shorcuts;
 	int carMenuOption;
+	int carTypeOption;
 	shorcuts.clearFunct;
 	cout << "Welcome to Offrent, the car rental service of your dreams!";
 	cout << "\nPress Any Key to Continue!";
@@ -26,6 +27,37 @@ int main() {
 		/*rental
 		section*/
 		case 1:  
+
+		shorcuts.clearFunct;
+
+		cout<<"\nwhat type of car do you want to rent?";
+		cout<<"\n1)Sedan";
+		cout<<"\n2)crossover/suv";
+		cout<<"\n3)minivan";
+		cout<<"\n4)sports car";
+
+		cin>>carTypeOption;
+		switch(carTypeOption){
+
+			/*Sedan rental
+			section*/
+			case 1:
+
+			/*suv rental
+			section*/
+			case 2:
+
+			/*mom car
+			section💀*/
+			case 3:
+
+
+			/*sports car
+			section*/
+			case 4:
+
+		}
+
 
 
 		/*Turn-In
